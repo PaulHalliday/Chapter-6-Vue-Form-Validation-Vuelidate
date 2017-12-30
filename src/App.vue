3532e83ea84bde479c4fe2f0147dd8579ce2ae65
Paul Halliday <hello@paulhalliday.io>
2017-12-30 20:52:43 +0000

    Added repeat password validation (not finished)

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,55 @@
       <div class="input">
         <label for="email">Email</label>
         <input 
+        :class="{ error: $v.email.$error }"
         type="email" 
         id="email" 
         @input="$v.email.$touch()"
         v-model.trim="email">
+        <p class="error-message"  v-if="!$v.email.email">Please enter a valid email address.</p>
+        <error-field-required :isRequired="!$v.email.required" name="Email" />
       </div>
       <div class="input"> 
         <label for="firstName">First Name</label>
         <input 
+        :class="{ error: $v.firstName.$error }"
         type="text"
         id="firstName" 
         v-model.trim="firstName"
         @input="$v.firstName.$touch()">
+        <error-field-required :isRequired="!$v.firstName.required" name="First Name" />
       </div>
       <div class="input">
         <label for="lastName">Last Name</label>
         <input 
+        :class="{ error: $v.lastName.$error}"
         type="text" 
         id="lastName" 
         v-model.trim="lastName"
         @input="$v.lastName.$touch()">
+        <error-field-required :isRequired="!$v.lastName.required" name="Last Name" />
       </div>
       <div class="input">
         <label for="password">Password</label>
         <input 
+        :class="{ error: $v.password.$error }"
         type="password" 
         id="password" 
         v-model.trim="password"
         @input="$v.password.$touch()">
+        <error-field-required :isRequired="!$v.password.required" name="Password" />
+      </div>
+      <div class="input">
+        <label for="password">Repeat Password</label>
+        <input 
+        :class="{ error: $v.repeatPassword.$error }"
+        type="password" 
+        id="password" 
+        v-model.trim="repeatPassword"
+        @input="$v.repeatPassword.$touch()">
+
+        <p class="error-message"  v-if="!$v.repeatPassword.sameAsPassword">Passwords must be identical.</p>
+        <error-field-required :isRequired="!$v.repeatPassword.required" name="Password" />
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -43,9 +64,23 @@
 </template>
 
 <script>
-import { required, email } from 'vuelidate/lib/validators';
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators';
+
+import Required from './components/Required';
 
 export default {
+  components: {
+    errorFieldRequired: Required,
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      repeatPassword: '',
+      firstName: '',
+      lastName: '',
+    };
+  },
   validations: {
     email: {
       required,
@@ -59,15 +94,11 @@ export default {
     },
     password: {
       required,
+      minLength: minLength(6),
     },
-  },
-  data() {
-    return {
-      email: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-    };
+    repeatPassword: {
+      sameAsPassword: sameAs('password'),
+    },
   },
   methods: {
     onSubmit() {},
@@ -95,5 +126,8 @@ input:focus {
 }
 .error {
   border: 1px solid red;
+}
+.error-message {
+  color: red;
 }
 </style>
---
+++ src/components/Required.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="error-message" v-if="!isRequired">{{name}} must not be empty.</p>
+</template>
+
+<script>
+export default {
+  props: ['isRequired', 'name'],
+};
+</script>
+
+<style>
+.error-message {
+  color: red;
+}
+</style>
