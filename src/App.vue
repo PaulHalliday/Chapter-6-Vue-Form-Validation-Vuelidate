525a10ceccb5526dcd48faf212d3140c931b1ac3
Paul Halliday <hello@paulhalliday.io>
2017-12-30 18:29:28 +0000

    Added require and email validators. Displayed output on right of form.

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <form class="form" @submit.prevent="onSubmit">
+      <div class="input">
+        <label for="email">Email</label>
+        <input 
+        :class="{ error: $v.email.$invalid}"
+        type="email" 
+        id="email" 
+        @input="$v.email.$touch()"
+        v-model.trim="email">
+      </div>
+      <div class="input"> 
+        <label for="firstName">First Name</label>
+        <input 
+        type="text"
+        id="firstName" 
+        v-model.trim="firstName"
+        @input="$v.firstName.$touch()">
+      </div>
+      <div class="input">
+        <label for="lastName">Last Name</label>
+        <input 
+        type="text" 
+        id="lastName" 
+        v-model.trim="lastName"
+        @input="$v.lastName.$touch()">
+      </div>
+      <div class="input">
+        <label for="password">Password</label>
+        <input 
+        type="password" 
+        id="password" 
+        v-model.trim="password"
+        @input="$v.password.$touch()">
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+    <div class="validators">
+      <pre>{{$v}}</pre>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators';
+
+export default {
+  validations: {
+    email: {
+      required,
+      email,
+    },
+    firstName: {
+      required,
+    },
+    lastName: {
+      required,
+    },
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+    };
+  },
+  methods: {
+    onSubmit() {},
+  },
+};
+</script>
+
+<style>
+.form {
+  display: inline-block;
+  text-align: center;
+  width: 49%;
+}
+.validators {
+  display: inline-block;
+  width: 49%;
+  text-align: center;
+  vertical-align: top;
+}
+.input {
+  padding: 5px;
+}
+input:focus {
+  outline: none;
+}
+.error {
+  border: 1px solid red;
+}
+</style>
