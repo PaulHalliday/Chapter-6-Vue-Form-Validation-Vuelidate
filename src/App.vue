46feec60af023e7da9de3306345194d5b45369f4
PaulHalliday <hello@paulhalliday.io>
2017-12-31 01:29:06 +0000

    Added validation for repeat password.

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,78 @@
 <template>
   <div>
     <form class="form" @submit.prevent="onSubmit">
-      <div class="input">
-        <label for="email">Email</label>
-        <input 
-        :class="{ error: $v.email.$error }"
-        type="email" 
-        id="email" 
-        @input="$v.email.$touch()"
-        v-model.trim="email">
-        <p class="error-message"  v-if="!$v.email.email">Please enter a valid email address.</p>
-        <error-field-required :isRequired="!$v.email.required" name="Email" />
-      </div>
-      <div class="input"> 
-        <label for="firstName">First Name</label>
-        <input 
-        :class="{ error: $v.firstName.$error }"
-        type="text"
-        id="firstName" 
-        v-model.trim="firstName"
-        @input="$v.firstName.$touch()">
-        <error-field-required :isRequired="!$v.firstName.required" name="First Name" />
-      </div>
-      <div class="input">
-        <label for="lastName">Last Name</label>
-        <input 
-        :class="{ error: $v.lastName.$error}"
-        type="text" 
-        id="lastName" 
-        v-model.trim="lastName"
-        @input="$v.lastName.$touch()">
-        <error-field-required :isRequired="!$v.lastName.required" name="Last Name" />
-      </div>
-      <div class="input">
-        <label for="password">Password</label>
-        <input 
-        :class="{ error: $v.password.$error }"
-        type="password" 
-        id="password" 
-        v-model.trim="password"
-        @input="$v.password.$touch()">
-        <error-field-required :isRequired="!$v.password.required" name="Password" />
-      </div>
-      <div class="input">
-        <label for="password">Repeat Password</label>
-        <input 
-        :class="{ error: $v.repeatPassword.$error }"
-        type="password" 
-        id="password" 
-        v-model.trim="repeatPassword"
-        @input="$v.repeatPassword.$touch()">
+<div class="input">
+  <label for="email">Email</label>
+  <input 
+  :class="{ error: $v.email.$error }"
+  type="email" 
+  id="email" 
+  @input="$v.email.$touch()"
+  v-model.trim="email">
 
-        <p class="error-message"  v-if="!$v.repeatPassword.sameAsPassword">Passwords must be identical.</p>
-        <error-field-required :isRequired="!$v.repeatPassword.required" name="Password" />
-      </div>
+  <div v-if="$v.email.$dirty">
+    <p class="error-message"  v-if="!$v.email.email">Please enter a valid email address.</p>
+    <p class="error-message" v-if="!$v.email.required">Email must not be empty.</p>
+  </div>
+
+</div>
+<div class="input"> 
+  <label for="firstName">First Name</label>
+  <input 
+  :class="{ error: $v.firstName.$error }"
+  type="text"
+  id="firstName" 
+  v-model.trim="firstName"
+  @input="$v.firstName.$touch()">
+  
+  <div v-if="$v.firstName.$dirty">
+    <p class="error-message" v-if="!$v.firstName.required">First Name must not be empty.</p>
+  </div>
+</div>
+<div class="input">
+  <label for="lastName">Last Name</label>
+  <input 
+  :class="{ error: $v.lastName.$error}"
+  type="text" 
+  id="lastName" 
+  v-model.trim="lastName"
+  @input="$v.lastName.$touch()">
+
+  <div v-if="$v.lastName.$dirty">
+    <p class="error-message" v-if="!$v.lastName.required">Last Name must not be empty.</p>
+  </div>
+</div>
+<div class="input">
+  <label for="password">Password</label>
+  <input 
+  :class="{ error: $v.password.$error }"
+  type="password" 
+  id="password" 
+  v-model.trim="password"
+  @input="$v.password.$touch()">
+
+  <div v-if="$v.password.$dirty">
+    <p class="error-message" v-if="!$v.password.required">Password must not be empty.</p>
+    <p class="error-message" v-if="!$v.password.minLength">Password needs to be at least 6 characters.</p>
+  </div>
+</div>
+<div class="input">
+  <label for="repeatPassword">Repeat Password</label>
+  <input 
+  :class="{ error: $v.repeatPassword.$error }"
+  type="password" 
+  id="repeatPassword" 
+  v-model.trim="repeatPassword"
+  @input="$v.repeatPassword.$touch()">
+
+  <div v-if="$v.repeatPassword.$dirty">
+    <p class="error-message" v-if="!$v.repeatPassword.minLength">Password needs to be at least 6 characters.</p>
+    <p class="error-message"  v-if="!$v.repeatPassword.sameAsPassword">Passwords must be identical.</p>
+    
+
+    <p class="error-message" v-if="!$v.repeatPassword.required">Password must not be empty.</p>
+  </div>
+</div>
       <button type="submit">Submit</button>
     </form>
     <div class="validators">
@@ -66,12 +85,7 @@
 <script>
 import { required, email, minLength, sameAs } from 'vuelidate/lib/validators';
 
-import Required from './components/Required';
-
 export default {
-  components: {
-    errorFieldRequired: Required,
-  },
   data() {
     return {
       email: '',
